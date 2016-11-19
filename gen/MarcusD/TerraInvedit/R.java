cb68517537475748ce5ae18ea81d71527ad1e3f3
MarcuzD <robonyunyu@gmail.com>
2016-11-19 14:35:35 +0100

    Workspace refactor

--- gen/MarcusD/TerraInvedit/R.java
+++ gen/MarcusD/TerraInvedit/R.java
@@ -26,35 +26,39 @@ public final class R {
         public static final int uwot=0x7f020001;
     }
     public static final class id {
+        public static final int action_settings=0x7f08000b;
         public static final int btnCancel=0x7f080004;
         public static final int btnSaev=0x7f080005;
         public static final int button1=0x7f080000;
-        public static final int button2=0x7f080002;
-        public static final int item1=0x7f08000b;
-        public static final int item2=0x7f08000c;
+        public static final int button2=0x7f080001;
+        public static final int item1=0x7f08000c;
+        public static final int item2=0x7f08000d;
         public static final int label=0x7f08000a;
         public static final int logo=0x7f080009;
         public static final int numBuff=0x7f080008;
         public static final int numItemID=0x7f080007;
         public static final int numItemname=0x7f080006;
         public static final int numItemnum=0x7f080003;
-        public static final int numberPicker1=0x7f080001;
+        public static final int numberPicker1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_items=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int dialog_pad=0x7f030002;
-        public static final int layout_itemedit=0x7f030003;
-        public static final int list_items=0x7f030004;
+        public static final int activity_keyedit=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int dialog_pad=0x7f030003;
+        public static final int layout_itemedit=0x7f030004;
+        public static final int list_items=0x7f030005;
     }
     public static final class menu {
-        public static final int menu_invedit=0x7f070000;
+        public static final int activity_keyedit=0x7f070000;
+        public static final int menu_invedit=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_label=0x7f050000;
         public static final int app_name=0x7f050001;
         public static final int button_getroot=0x7f050006;
+        public static final int button_setkey=0x7f05001b;
         public static final int javatrans_auto=0x7f050017;
         public static final int javatrans_dialog_loader=0x7f05000d;
         public static final int javatrans_dialog_save=0x7f05000e;
@@ -75,9 +79,11 @@ public final class R {
         public static final int menu_discard=0x7f050007;
         public static final int menu_pad=0x7f050009;
         public static final int menu_saev=0x7f050008;
-        public static final int title_activity_activity_players=0x7f050003;
+        public static final int title_activity_activity_players=0x7f05001d;
         public static final int title_activity_invedit=0x7f050004;
         public static final int title_activity_itemedit=0x7f050005;
+        public static final int title_activity_keyedit=0x7f05001c;
+        public static final int title_activity_players=0x7f050003;
     }
     public static final class style {
         /** 
